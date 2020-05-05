1e66a1b63adeeafa8367f0c6d0f5eaa8662a50cd
Nicholas McHenry <nicholasmchenry@MacBook-Pro.local>
2020-05-05 09:26:44 -0400

    scaffold todo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TodoModule],
 })
 export class AppModule {}
